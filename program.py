9322297e3926f796aa381c25c9db49f5beacffb4
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2024-08-23 18:46:10 +0000

    HW First Commit

--- program.py
+++ program.py
@@ -15,5 +15,5 @@ elif num == 0:
    print("The factorial of 0 is 1")
 else:
    for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is,factorial)
+       factorial = factorial*
+   print("The factorial of",num,"is",factorial)
