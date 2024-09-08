06563491dffa10c56931f0b8215eb209b680e6dc
Leslie Liu <s9172051212@gmail.com>
2024-09-08 23:09:40 +0000

    edit hw2 file to meet pylint requirements

--- hw2_rsc/rand.py
+++ hw2_rsc/rand.py
@@ -1,8 +1,18 @@
+"""
+This module provides functionality for generating random arrays using subprocess.
+"""
 import subprocess
 
 def random_array(arr):
+    """
+    Fills the input list with random numbers using the `shuf` command.
+    Args:
+        arr (list): A list that will be filled with random integers.
+    Returns:
+        list: The input list with random integers.
+    """
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i, _ in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
