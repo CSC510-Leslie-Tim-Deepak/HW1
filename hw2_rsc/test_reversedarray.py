e398ca20624a823310a09ffe032933b6c22a2f8a
Leslie Liu <s9172051212@gmail.com>
2024-09-09 01:55:06 +0000

    edit test case to meet bandit requirement

--- hw2_rsc/test_reversedarray.py
+++ hw2_rsc/test_reversedarray.py
@@ -1,4 +1,10 @@
+import unittest
 from hw2_debugging import merge_sort
 
-def test_reverse_sorted_array():
-    assert merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+
+class TestMergeSort(unittest.TestCase):
+    def test_reverse_sorted_array(self):
+        self.assertEqual(merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5])
+
+if __name__ == '__main__':
+    unittest.main()
