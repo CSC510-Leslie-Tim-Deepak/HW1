862b55c5e31e82c997adba6496e8fab70b8c5b3c
Captain-Tim <62422680+Captain-Tim@users.noreply.github.com>
2024-09-11 00:31:21 -0400

    Update rand.py
    add error

--- hw2_rsc/rand.py
+++ hw2_rsc/rand.py
@@ -1,17 +1,9 @@
-"""
-This module provides functionality for generating random arrays using subprocess.
-"""
+
 import secrets
 
 
 def random_array(arr):
-    """
-    Fills the input list with random numbers using the `shuf` command.
-    Args:
-        arr (list): A list that will be filled with random integers.
-    Returns:
-        list: The input list with random integers.
-    """
+
     for i, _ in enumerate(arr):
         arr[i] = secrets.randbelow(20) + 1
     return arr
